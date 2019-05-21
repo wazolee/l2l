6c96bd562584d1b2e67eedf89caa505a9254932a
VZ <wazolee@gmail.com>
2019-05-21 20:46:56 +0200

    # my first issue commit

---
+++ l2l.Data/Model/Course.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace l2l.Data.Model
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ l2l.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace l2l.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
